66e527ae786967f96e087b205dce11f7f23c6afb
Your Name <ahmet.dikbayir@gmail.com>
2024-05-03 23:33:49 +0300

    getAll method and createStudent method created

--- springbootintro/src/main/java/com/tpe/repository/StudentRepository.java
+++ springbootintro/src/main/java/com/tpe/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import com.tpe.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository //kod okunabilirliğini arttırmak
+@Repository //optional -- > kod okunabilirliğini arttırmak
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByEmail(String email);
 }
--- springbootintro/src/main/java/com/tpe/service/StudentService.java
+++ springbootintro/src/main/java/com/tpe/service/StudentService.java
@@ -1,8 +1,12 @@
 package com.tpe.service;
 
+import com.tpe.domain.Student;
+import com.tpe.exception.ConflictException;
 import com.tpe.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -10,5 +14,18 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+//Not: getAll() ************************************************
+    public List<Student> getAll() {
+        //!!!handle etmemiz gereken bir durum var mı? YOK
+        return studentRepository.findAll(); // SELECT * FROM student
+    }
 
+    public void createStudent(Student student) {
+
+        //!!! kontrol etmemiz gereken bir durum var mi? Email unique mi kontrolü
+        if(studentRepository.existsByEmail(student.getEmail())){
+            throw new ConflictException("Email is already exist");
+        }
+        studentRepository.save(student);
+    }
 }
